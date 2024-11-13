105a2612af216fea22039f7964fd35e9aa1afc31
Sakhile Ndlazi <sakhilelindah@gmail.com>
2024-11-13 10:55:32 +0200

    Add fizzbuzz implementation.

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+x, y, n = list(map(int, stdin.readline().split()))
+
+for i in range(1, n + 1):
+    if i % x == 0 and i % y == 0:
+        print("FizzBuzz")
+    elif i % y == 0:
+        print("Buzz")
+    elif i % x == 0:
+        print("Fizz")
+    else:
+        print(i)
